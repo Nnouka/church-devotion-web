ef22852bab62409483016b7931723fadbcbf17da
Nouks <nnouka.stephen@go-groups.net>
2020-05-23 13:50:24 +0100

    Chore: update dashboard

--- src/home/DashBoard.js
+++ src/home/DashBoard.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import MainHeader from './MainHeader';
 import * as TRANS from '../utils/trans/TranslationService';
+import * as AppUtils from '../utils/AppUtils';
 
 class DashBoard extends Component {
     render() {
@@ -10,6 +11,10 @@ class DashBoard extends Component {
                 <MainHeader lang={lang} active={{route: '/dashboard'}} onLogout={onLogout}/>
                 <div className="mt-50">
                     {`<${TRANS.trans('cnt_contact', lang)}/>`}
+                </div>
+                <div className="mt-50">
+                    {`<${TRANS.trans('welcome')} ${AppUtils.getUserDetails() === undefined ||
+                    AppUtils.getUserDetails() === null ? '' : AppUtils.getUserDetails().fullName}>`}
                 </div>
             </div>
         );
